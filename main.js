5b3f502f201ed329b10d10994838683aef54b9c3
Giulia <giulia.guerrazzi@live.it>
2020-10-30 16:19:06 +0100

    add exercise punto 2

---
+++ main.js
@@ -0,0 +1,57 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare attraverso il for..in tutte le proprietà.
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente all'array creato al punto 2 inserendo nell’ordine: nome, cognome e età.
+
+var studentGiulia = {
+  nome: 'Giulia',
+  cognome: 'Guerrazzi',
+  età: 30,
+}
+
+for (var key in studentGiulia) {
+console.log(studentGiulia[key]);
+}
+
+var studentSara = {
+  nome: 'Sara',
+  cognome: 'Rossi',
+  età: 20,
+}
+
+var studentGianluca = {
+  nome: 'Gianluca',
+  cognome: 'Cioni',
+  età: 25,
+}
+
+var studentChiara = {
+  nome: 'Chiara',
+  cognome: 'Cipriani',
+  età: 28,
+}
+
+var classe = [
+  {
+    nome: 'Sara',
+    cognome: 'Rossi',
+    età: 20,
+  },
+  {
+    nome: 'Gianluca',
+    cognome: 'Cioni',
+    età: 25,
+  },
+  {
+    nome: 'Chiara',
+    cognome: 'Cipriani',
+    età: 28,
+  },
+]
+
+for (var i = 0; i < classe.length; i++) {
+  console.log(classe[i]);
+  console.log(classe[i]['nome']);
+  console.log(classe[i]['cognome']);
+  }
+
+var newStudent = [];
